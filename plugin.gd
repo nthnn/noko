0d4fd59cfe4ca93e629004c2a223cc84e4f9b97d
Nathanne Isip <nathanneisip@gmail.com>
2025-04-19 01:40:14 +0800

    Copyright, license header, and added NokoBlobs class to main addon plugin script.

--- plugin.gd
+++ plugin.gd
@@ -1,12 +1,19 @@
+# Copyright 2025 Nathanne Isip
+# This file is part of Noko (https://github.com/nthnn/noko)
+# This code is licensed under MIT license (see LICENSE for details)
+
 extends EditorPlugin
 
+var NokoBlobs = preload("res://modules/NokoBlobs.gd").new()
 var NokoModel = preload("res://modules/NokoModel.gd").new()
 var NokoPrompt = preload("res://modules/NokoPrompt.gd").new()
 
 func _enter_tree() -> void:
+    Engine.register_singleton("NokoBlobs", NokoBlobs)
     Engine.register_singleton("NokoModel", NokoModel)
     Engine.register_singleton("NokoPrompt", NokoPrompt)
 
 func _exit_tree() -> void:
+    Engine.unregister_singleton("NokoBlobs")
     Engine.unregister_singleton("NokoModel")
     Engine.unregister_singleton("NokoPrompt")
