27663b1ff71b422ea2cf7f68978744b2424c1522
Nathanne Isip <nathanneisip@gmail.com>
2025-04-19 02:58:00 +0800

    NokoModel.delete_model() function implementation

--- modules/NokoModel.gd
+++ modules/NokoModel.gd
@@ -233,3 +233,25 @@ static func copy_model(
         },
         use_ssl
     )
+
+static func delete_model(
+    parent: Node,
+    server: Dictionary,
+    model: String,
+    use_ssl: bool = true
+)-> Dictionary:
+    if (!server.has("host") or
+        !server.has("port")):
+        push_error("Server host name and port number must be defined")
+        return {"result": 0}
+
+    return await NetUtils.send_post_request(
+        parent,
+        server["host"] + ":" + str(server["port"]) + "/api/delete",
+        {
+            "User-Agent": "noko-godot/0.0.1",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        {"model": model},
+        use_ssl
+    )
